c670d885afe05f10aed45a77c0578b36cc7a496d
xenfeild <antonio.t350@gmail.com>
2023-05-04 10:21:42 +0200

    opérationel-commit

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,9 +1,13 @@
 import Link from 'next/link';
 import React from 'react';
+// import './navBar.css';
 
 export default function Navbar() {
   return (
     <div>
+      <div className='logo'>
+        <h1>redd<span>it</span></h1>
+      </div>
         <ul>
             <Link href='/'>
                 <li>Home</li>
@@ -21,3 +25,5 @@ export default function Navbar() {
     </div>
   );
 }
+
+
